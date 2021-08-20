b4e9aec5e39b3caec96a9c46aba8de68094bcc3a
AlexisLok <a01251886@itesm.mx>
2021-08-20 16:26:02 +0000

    Ejercicio 1 de Quiz 1

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -2,5 +2,13 @@ import math
 def main():
     #escribe tu código abajo de esta línea
 
+    x1= int(input("x1= "))
+    y1= int(input("y1= "))
+    x2= int(input("x2= "))
+    y2= int(input("y2= "))
+
+    distancia = round(float(math.sqrt((x2 - x1)**2 + (y2-y1)**2)),2)
+
+    print (f"distancia= {distancia}")
 if __name__ == '__main__':
     main()
